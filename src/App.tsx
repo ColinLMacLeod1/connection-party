67e37bc2b5218f2cd7880fe6510e20dbc20c8514
Colin MacLeod <colinlmacleod1@gmail.com>
2025-10-12 11:31:53 -0400

    added main page and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const buttonClick = () => {
+    const test = count;
+    setCount(test);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={"bg-white"}>
+        <div className={"relative isolate px-6 pt-14 lg:px-8"}>
+          <div className={"mx-auto max-w-2xl py-32 sm:py-48 lg:py-56"}>
+            <div className={"text-center"}>
+              <h1
+                className={
+                  "text-5xl font-semibold tracking-tight text-balance text-black sm:text-7xl"
+                }
+              >
+                Connection Party!
+              </h1>
+              <p
+                className={
+                  "mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"
+                }
+              >
+                Join or Create a room:
+              </p>
+              <div className={"mt-10 flex items-center justify-center gap-x-6"}>
+                <a
+                  href="#"
+                  className={
+                    "font-tile rounded-md bg-theme-beige px-3.5 py-2.5 text-sm font-semibold text-black shadow-xs hover:bg-theme-blue hover:scale-105 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bg-theme-blue"
+                  }
+                  onClick={buttonClick}
+                >
+                  Join Room
+                </a>
+                <a
+                  href="#"
+                  className={
+                    "font-tile rounded-md bg-theme-beige px-3.5 py-2.5 text-sm font-semibold text-black shadow-xs hover:bg-theme-purple hover:scale-105 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-theme-purple"
+                  }
+                  onClick={buttonClick}
+                >
+                  Create Room
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
